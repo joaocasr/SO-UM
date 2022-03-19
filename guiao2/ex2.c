843b6e19cb8c6c45134b0a4b049a72e2145b2edf
joaocasr <castrojoao839@gmail.com>
2022-03-19 03:02:55 +0000

    resolucoes guiao2

--- guiao2/ex2.c
+++ guiao2/ex2.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
-#define SIZE 100
+#define SIZE 1024*1024
 /*STDERR_FILENO
     File number of stderr; 2.
 STDIN_FILENO
@@ -27,20 +27,21 @@ int main(int argc, char * argv[]){
   pid_t pid,ant;
   pid = fork();// o fork devolve o pid do filho
   char buffFilho[SIZE];
-  int i=0;
+  // int i=0;
   char buffPai[SIZE];
   if(pid==0){ // fork retorna 0 ao processo filho
 	  //Código do filho
-	i++;
-	printf("variavel nao partilhada filho i=%d\n",i);
+	//i++;
+	//printf("variavel nao partilhada filho i=%d\n",i);
 	sprintf(buffFilho, "[Fillo]Pid:%d , [Filho] Pid pai:%d\n",getpid(),getppid());
 	int tam = countBytes(buffFilho,sizeof(buffFilho));
 	write(1,buffFilho,tam);
   }else{//apos o filho terminar
 	  //Código do pai
-	  //como nao tem um wait o filho vai ficar orfao
-        i--;
-	printf("variavel nao partilhada pai i=%d\n",i);
+	  //como nao tem um wait o filho vai ficar orfao//pai acaba mais rapido
+        //na situacao do pai nao recolher o valor do filho e o filho terminar a sua execucao, o fiho entra em estado zombie. Quando o pai deixar de estar bloqueado(em sleep), apos esse tempo acorda e termina a sua execuçao, recolhendo o valor do filho e o filho deixa de estar em estado zombie.
+	  //i--;
+	//printf("variavel nao partilhada pai i=%d\n",i);
 	sprintf(buffPai, "[Pai] Pid filho:%d , [Pai] Pid pai:%d [Pai] Pid: %d\n",pid,getpid(),getppid());
         int tam = countBytes(buffPai,sizeof(buffPai));
         write(1,buffPai,tam);
